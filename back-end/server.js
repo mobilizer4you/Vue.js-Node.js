70ee929cf5124ec01debc07ceedd1bccfb799c78
D.M <d.m.risingdeveloper@gmail.com>
2020-04-20 17:57:57 -0500

    Created Node.js Todolist API

--- back-end/server.js
+++ back-end/server.js
@@ -1,22 +1,22 @@
-﻿require('rootpath')();
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const errorHandler = require('./_helpers/error-handler');
+var express = require('express');
+    app = express(),
+    port = process.env.PORT || 3000;
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todolistModel'),
+    bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(cors());
-
-// api routes
-app.use('/users', require('./users/users.controller'));
-
-// global error handler
-app.use(errorHandler);
-
-// start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+app.use(function(req, res) {
+    res.status(404).send({url:req.originalUrl} + 'not found')
 });
+
+var routes = require('./api/routes/todolistRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log('todolist RESTful API server started on: ' + port);
